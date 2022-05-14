14839371204ccb24b5521873a0bfa0aca5281cda
Difinition <vladdifinition@mail.ru>
2022-05-14 19:46:00 +0300

    RN-1 Todo list

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList, Text } from "react-native";
+import Header from "./components/Header";
+import ListItem from "./components/ListItem";
+import Form from "./components/Form";
 
 export default function App() {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        // загрузка по сети
+    }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+        <Header title={"Список дел"} />
+
+        <FlatList
+            contentContainerStyle={{ paddingBottom: 20 }}
+            style={styles.list}
+            data={tasks}
+            renderItem={({ item }) => (
+                <ListItem key={item.text} item={item} />
+            )}
+        />
+
+        <Form setTasks={setTasks} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    main: {
+        flex: 1,
+        backgroundColor: "#e5e5e5",
+        paddingBottom: 140,
+    },
+    list: {
+        paddingHorizontal: 24,
+        paddingTop: 24,
+        paddingBottom: 60,
+    }
 });
