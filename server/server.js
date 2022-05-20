6f00caba78ee7086c5bb8ddd50a1cc880a85ce5a
Difinition <vladdifinition@mail.ru>
2022-05-20 21:22:59 +0300

    RN Добавление обработки ошибок на разные типы ext

--- server/server.js
+++ server/server.js
@@ -50,8 +50,18 @@ app.get('/file', (req, res) => {
 });
 
 // РЕДИРЕКТ НЕЗАРЕГИСТРИРОВАННЫХ МАРШРУТОВ
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// и проверка типа файлов
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({
+            error: 'Not find JSON-file',
+        })
+    } else {
+        res.type('txt').send('404 Not Found!');
+    }
 });
 
 app.use(errorHandler);
