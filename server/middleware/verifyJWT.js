55aaedce92abd44ce674cf5f8ee6e9fc4f8db864
Difinition <vladdifinition@mail.ru>
2022-06-13 22:15:06 +0300

    Verify JWT

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -13,7 +13,8 @@ const verifyJWT = (req, res, next) => {
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if (err) return res.sendStatus(403); // invalid token
-            req.user = decoded.username;
+            req.user = decoded.UserInfo.username;
+            req.roles = decoded.UserInfo.roles;
             next();
         }
     );
