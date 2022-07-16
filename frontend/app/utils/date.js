3d51ad002bac5474718fce5ee38621770f3b6b23
Difinition <vladdifinition@mail.ru>
2022-07-16 11:54:47 +0300

    FC groupBy update

--- frontend/app/utils/date.js
+++ frontend/app/utils/date.js
@@ -1,27 +1,20 @@
 const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
 
-const getFormatedDate = (dateStr) => {
+export const getFormatedDate = (dateStr) => {
     const date = new Date(dateStr);
     return `${date.getDate()} ${months[date.getMonth()]}`;
 };
 
 const arr = [];
 
-const newDateObj = arr.reduce((acc, cur) => {
+// 
+const groupByDate = (arr) => arr.reduce((acc, cur) => {
     const time = getFormatedDate(cur.fixture.date);
     acc[time] = acc[time] || {
         date: time,
-        fixture: [],
-        league: [],
-        teams: [],
-        goals: [],
-        score: [],
+        matches: [],
     }
 
-    acc[time].fixture.push(cur.fixture);
-    acc[time].league.push(cur.league);
-    acc[time].teams.push(cur.teams);
-    acc[time].goals.push(cur.goals);
-    acc[time].score.push(cur.score);
-    return acc;
+    acc[time].matches.push(cur);
+    return Object.values(acc);
 }, {});
